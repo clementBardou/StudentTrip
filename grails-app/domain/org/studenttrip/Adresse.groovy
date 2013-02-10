f02103ef6605db8aff4896fd12ba58c9cfacdef4
Benkhallouf Youssef <benkhalloufyoussef@gmail.com>
2013-02-10 13:13:26 +0100

    Constraint for Classe Adresse
    First Commit!!

--- grails-app/domain/org/studenttrip/Adresse.groovy
+++ grails-app/domain/org/studenttrip/Adresse.groovy
@@ -8,5 +8,14 @@ class Adresse {
 	int code_postal
 
     static constraints = {
+		rue(blank:false, nullable:false)
+		nom(blank:false, nullable:false)
+		ville(blank:false, nullable:false)
+		code_postal(max:99999)
     }
+	
+	String toString() {
+		
+		return rue + nom + ville + code_postal
+	}
 }
