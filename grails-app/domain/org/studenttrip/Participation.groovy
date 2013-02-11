149b9b99b1fa8b445f9f8b7771a0268ce9d70da7
Benkhallouf Youssef <benkhalloufyoussef@gmail.com>
2013-02-12 00:23:33 +0100

    methode equals pour Participation

--- grails-app/domain/org/studenttrip/Participation.groovy
+++ grails-app/domain/org/studenttrip/Participation.groovy
@@ -1,10 +1,20 @@
 package org.studenttrip
 
+import org.hibernate.loader.custom.Return;
+
 class Participation {
-	
+
 	EtatParticipation etat
 
-    static constraints = {
-		etat(nullable: false)
-    }
+	static constraints = { etat(nullable: false) }
+	@Override
+	public boolean equals(Object obj) {
+		Participation participation=(Participation) obj
+		return this.etat.equals(participation.etat)
+	}
+
+	@Override
+	public String toString() {
+		return etat
+	}
 }
